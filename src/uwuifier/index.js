12ba3c4851a8d8f16244d0e3cbe0fa382866c08b
Schotsl <sjorsvanholst@gmail.com>
2020-06-14 12:59:14 +0200

    Fixed non funciton eslint ignore lines

--- src/uwuifier/index.js
+++ src/uwuifier/index.js
@@ -1,10 +1,8 @@
 'use strict';
-
-/* eslint no-use-before-define: 0 */  // --> OFF
-
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    // eslint-disable-next-line
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
@@ -161,5 +159,3 @@ let Uwuifier = /** @class */ (() => {
     return Uwuifier;
 })();
 export { Uwuifier };
-
-/* eslint no-use-before-define: 2 */  // --> ON
