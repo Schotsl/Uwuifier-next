08b90d5423a238a36e68466d889f5e3b09ac3188
Schotsl <sjorsvanholst@gmail.com>
2020-06-15 13:07:37 +0200

    Made site responsive

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,42 @@
     <Logo></Logo>
 
     <div class="container">
-      <Textarea class="child left" type="input" :value="input" @change="updateInput"></Textarea>
-      <Textarea class="child right" type="output" :value="output"></Textarea>
+      <Textarea class="child" type="input" :value="input" @change="updateInput"></Textarea>
+
+      <div class="spacer"></div>
+
+      <Textarea class="child" type="output" :value="output"></Textarea>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
 
-import Textarea from './components/Textarea.vue';
-import Logo from './components/Logo.vue';
+  import Textarea from './components/Textarea.vue';
+  import Logo from './components/Logo.vue';
 
-import { Uwuifier } from './uwuifier/index';
+  import { Uwuifier } from './uwuifier/index';
 
-@Component({
-  components: {
-    Textarea,
-    Logo
+  @Component({
+    components: {
+      Textarea,
+      Logo
+    }
+  })
+
+  export default class App extends Vue {
+    private input = `This site can help you make any old boring text nice and uwu? We can't imagen anyone would actually use this but you gotta do what you gotta do`;
+
+    get output() {
+      const uwuifier = new Uwuifier();
+      return uwuifier.uwuifySentence(this.input);
+    }
+
+    updateInput(value: string) {
+      this.input = value;
+    }
   }
-})
-
-export default class App extends Vue {
-  private input = `This site can help you make any old boring text nice and uwu? We can't imagen anyone would actually use this but you gotta do what you gotta do`;
-
-  get output() {
-    const uwuifier = new Uwuifier();
-    return uwuifier.uwuifySentence(this.input);
-  }
-
-  updateInput(value: string) {
-    this.input = value;
-  }
-}
 </script>
 
 <style>
@@ -45,34 +48,54 @@ export default class App extends Vue {
   }
 
   #app {
-    background-color: rgba(255, 203, 45, 1);
-    height: 100%;
     color: #2c3e50;
-
-    font-family: 'Quicksand', sans-serif;
-    font-weight: 400;
+    height: 100%;
     text-align: center;
+    background-color: rgba(255, 203, 45, 1);
+
+    font-weight: 400;
+    font-family: 'Quicksand', sans-serif;
+
+    display: flex;
+    flex-flow: column;
   }
 
   .container {
-    height: 50%;
+    padding: 5%;
+    height: 100%;
+
     display: flex;
-    justify-content: space-between;
+    flex: 1 1 auto;
+    flex-direction: column;
   }
 
   .child {
-    width: 50%;
-    height: 100%;
+    width: 100%;
+    height: 47.5%;
 
     display: flex;
     flex-direction: column; 
   }
 
-  .left {
-    margin: 0 1.25% 0 2.5%;
+  .spacer {
+    width: 100%;
+    height: 5%;
   }
 
-  .right {
-    margin: 0 2.5% 0 1.25%;
+  @media only screen and (min-width: 768px) {
+    .container {
+      padding: 2.5%;
+      flex-direction: row;
+    }
+
+    .spacer {
+      width: 2.5%;
+      height: 100%;
+    }
+
+    .child {
+      height: 100%;
+      width: 48.75%;
+    }
   }
 </style>
