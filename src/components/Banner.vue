fa6b13c87de62c3ba8cd2022cd91844249a33b2f
DrunkWinter <na.kerimdemirci@gmail.com>
2020-06-16 16:21:50 +0200

    UI overhaul

---
+++ src/components/Banner.vue
@@ -0,0 +1,70 @@
+<template>
+    <section id="banner">
+        <div class="container">
+            <div class="image">🥱</div>
+            <div class="content">
+                <span><b>Uwuifier</b> the best tool when it comes to...</span>
+                <div id="buttons">
+                    <a href="">GET STARTED</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    #banner .container {
+        justify-content: center;
+        height: 350px;
+        width: 900px;
+        margin-top: calc(var(--header-height) * -1 + 20px) !important;
+    }
+
+    #banner .image {
+        font-size: 10rem;
+    }
+
+    #banner .content {
+        margin: 30px 0 0 40px;
+    }
+
+    #banner span {
+        font-size: 3rem;
+        font-weight: 300;
+        margin-bottom: 20px;
+        display: block;
+    }
+
+    #banner a {
+        color: #000;
+        background: var(--app-accent);
+        border-radius: 30px;
+        padding: 15px 20px;
+        font-weight: 600;
+        transition: background 0.25s ease, box-shadow 0.25s ease;
+    }
+
+    #banner a:hover {
+        background: rgba(255, 200, 61, 0.80);
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    }
+
+    @media screen and (max-width: 900px) {
+        #banner .container {
+            height: auto;
+            margin: 0 60px;
+        }
+        #banner .content {
+            margin: 0;
+            margin-top: 20px;
+        }
+        #banner span {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        #banner #buttons {
+            display: flex;
+            justify-content: center;
+        }
+    }
+</style>
---
+++ src/components/Demo.vue
@@ -0,0 +1,127 @@
+<template>
+    <section id="demo" class="darker">
+        <div class="container">
+            <div class="input">
+                <div class="legend">
+                    <span>Input</span>
+                </div>
+                <Textarea class="child" type="input" :value="input" :typing="typing" @change="updateInput"></Textarea>
+            </div>
+            <div class="output">
+                <div class="legend">
+                    <span>Output</span>
+                </div>
+                <Textarea class="child" type="output" :value="output" :typing="typing"></Textarea>
+                <button>♻</button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  import { Uwuifier } from '../uwuifier/index';
+
+  import Textarea from './Textarea.vue';
+
+  @Component({
+    components: {
+      Textarea
+    }
+  })
+  export default class Demo extends Vue {
+    private input = `Hey! This site can help you make any old boring text nice and uwu? We can't imagen anyone would actually use this but you gotta do what you gotta do.`;
+    private typing = false;
+
+    get output() {
+      const uwuifier = new Uwuifier();
+      return uwuifier.uwuifySentence(this.input);
+    }
+
+    updateInput(value: string) {
+      this.input = value;
+    }
+  }
+</script>
+
+<style scoped>
+    #demo .container {
+        position: relative;
+        height: 500px;
+        width: 1100px;
+        box-shadow: 0 20px 25px 10px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+        border-radius: 6px;
+        overflow: hidden;
+    }
+
+    #demo .input,
+    #demo .output {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        height: 100%;
+        width: 100%;
+    }
+
+    #demo .input {
+        flex: 4;
+    }
+
+    #demo .output {
+        flex: 5;
+    }
+
+    #demo .input textarea {
+        background: var(--demo-io-background);
+    }
+
+    #demo .output textarea {
+        background: var(--app-accent);
+        color: #000;
+    }
+
+    #demo .legend {
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: var(--demo-io-background);
+        font-weight: 600;
+        height: 40px;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+    }
+
+    @media screen and (min-width: 901px) {
+        #demo .input .legend::before {
+            content: '↔';
+            position: absolute;
+            background: inherit;
+            top: 0;
+            right: -10px;
+            font-size: 1.2rem;
+            height: 100%;
+            width: 20px;
+            display: flex;
+            align-items: center;
+            z-index: 1;
+        }
+    }
+
+    #demo .output button {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        margin: 20px;
+        width: 40px;
+        height: 40px;
+        font-size: 1.4rem;
+        color: #3a3a3a;
+        border-radius: 50%;
+        transition: background 0.25s ease;
+    }
+
+    #demo .output button:hover {
+        background: rgba(0, 0, 0, 0.15);
+    }
+</style>
