bb8f1750dd33b15e7fb61c13b77d10925e34ddb0
Sjors van Holst <sjorsvanholst@gmail.com>
2023-10-19 18:42:30 +0200

    📈 Added event points

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -4,6 +4,7 @@ import IntroDemo from "@/components/Intro/Demo";
 import IntroHeader from "@/components/Intro/Header";
 
 import { useState } from "react";
+import { usePlausible } from "next-plausible";
 
 type IntroProps = {
   initial: number;
@@ -18,6 +19,8 @@ export default function Intro({ initial }: IntroProps) {
     return personalParsed;
   });
 
+  const plausible = usePlausible();
+
   function onUwuified() {
     setOffset((offset) => offset + 1);
     setPersonal((personal) => personal + 1);
@@ -26,6 +29,12 @@ export default function Intro({ initial }: IntroProps) {
     const personalStringified = personalIncreased.toString();
 
     localStorage.setItem("personal", personalStringified);
+
+    if (personalIncreased == 25) plausible("Uwuified 25 sentences");
+    if (personalIncreased == 50) plausible("Uwuified 50 sentences");
+    if (personalIncreased == 100) plausible("Uwuified 100 sentences");
+    if (personalIncreased == 250) plausible("Uwuified 250 sentences");
+    if (personalIncreased == 500) plausible("Uwuified 500 sentences");
   }
 
   return (
