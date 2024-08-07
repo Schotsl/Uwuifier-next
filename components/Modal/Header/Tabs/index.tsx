2500d3c6141b7e260010a813338faa3f4b2530bc
Sjors van Holst <sjorsvanholst@gmail.com>
2024-08-07 19:20:41 +0200

    📱 Made selection responsive

--- components/Modal/Header/Tabs/index.tsx
+++ components/Modal/Header/Tabs/index.tsx
@@ -1,4 +1,7 @@
 import styles from "./ModalTabs.module.scss";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
 
 type ModelHeaderTabsProps = {
   active: string;
@@ -15,9 +18,54 @@ export default function ModelHeaderTabs({
   active,
   onActive,
 }: ModelHeaderTabsProps) {
+  const [open, setOpen] = useState(false);
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMobile(window.innerWidth < 640);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleClick = (value: string) => {
+    if (isMobile) {
+      if (open && value === active) {
+        setOpen(false);
+      } else if (open && value !== active) {
+        onActive(value);
+        setOpen(false);
+      } else if (!open) {
+        setOpen(true);
+      }
+
+      return;
+    }
+
+    onActive(value);
+  };
+
+  const tabsCopy = [...tabs];
+  const tabsSorted = isMobile
+    ? tabsCopy.sort((a, b) =>
+        a.value === active ? -1 : b.value === active ? 1 : 0
+      )
+    : tabs;
+
   return (
-    <menu className={styles.tabs}>
-      {tabs.map((tab) => (
+    <menu
+      className={
+        open ? `${styles.tabs} ${styles["tabs--open"]}` : `${styles.tabs}`
+      }
+    >
+      {tabsSorted.map((tab, index) => (
         <li
           key={tab.value}
           className={
@@ -28,9 +76,16 @@ export default function ModelHeaderTabs({
         >
           <button
             className={styles.tabs__tab__button}
-            onClick={() => onActive(tab.value)}
+            onClick={() => handleClick(tab.value)}
           >
             {tab.label}
+
+            {index === 0 && (
+              <FontAwesomeIcon
+                icon={open ? faChevronUp : faChevronDown}
+                className={styles.tabs__tab__button__arrow}
+              />
+            )}
           </button>
         </li>
       ))}
