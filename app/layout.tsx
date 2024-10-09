5316456c824063b3a1ed8ed4113c2e6c092b675f
Sjors van Holst <sjorsvanholst@gmail.com>
2024-10-09 21:02:58 +0200

    ♻️ Forced static

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ const title = "Uwuifier Translator";
 const image = "https://uwuifier.com/banner-1200x630.png";
 const description = `Discover the ultimate app and website to transform your text with fun and cute uwu and owo styles. Uwuifier, also known as the Owoifier Translator, is the most advanced uwu and owo text generator on the web. Customize your translations to convert any text, sentence, or word into adorable, stutter-filled, and emotive phrases. Excluding URLs and @mentions, this tool provides a wide range of kawaii options. Plus, it offers an un-uwuifier feature to filter and revert any annoying uwuified texts from your friends back to normal. Perfect for all your weeb and playful text transformation needs!`;
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-static";
 export const metadata: Metadata = {
   title,
   keywords: [
