c4abaa0f90f60c91f455da5ac99a6b9f21c9885d
Sjors van Holst <sjorsvanholst@gmail.com>
2024-11-13 21:40:15 +0100

    ♻️ Disabled Plausible proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const { withPlausibleProxy } = require("next-plausible");
 const { withSentryConfig } = require("@sentry/nextjs");
 
 // Cut off at sentry.io
@@ -14,7 +13,7 @@ const contentSecurityPolicy = `
   worker-src 'self' blob:;
   object-src 'none';
   script-src 'self' blob: 'unsafe-inline' 'unsafe-eval';
-  connect-src 'self' data: ${reportURLStripped} wss://rqautahsvsoneozemjth.supabase.co https://rqautahsvsoneozemjth.supabase.co;
+  connect-src 'self' data: ${reportURLStripped} https://plausible.io wss://rqautahsvsoneozemjth.supabase.co https://rqautahsvsoneozemjth.supabase.co;
   report-to csp-endpoint;
   report-uri ${reportURLStripped};
   upgrade-insecure-requests;
@@ -30,41 +29,37 @@ const reportTo = {
   ],
 };
 
-module.exports = withPlausibleProxy({
-  customDomain: "https://plausible.hedium.nl",
-})(
-  withSentryConfig(
-    {
-      async headers() {
-        return [
-          {
-            source: "/(.*)",
-            headers: [
-              {
-                key: "Content-Security-Policy",
-                value: contentSecurityPolicy.replace(/\n/g, ""),
-              },
-              {
-                key: "Report-To",
-                value: JSON.stringify(reportTo),
-              },
-            ],
-          },
-        ];
-      },
+module.exports = withSentryConfig(
+  {
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: contentSecurityPolicy.replace(/\n/g, ""),
+            },
+            {
+              key: "Report-To",
+              value: JSON.stringify(reportTo),
+            },
+          ],
+        },
+      ];
     },
-    {
-      org: process.env.NEXT_PUBLIC_SENTRY_ORG,
-      project: process.env.NEXT_PUBLIC_SENTRY_PROJECT,
+  },
+  {
+    org: process.env.NEXT_PUBLIC_SENTRY_ORG,
+    project: process.env.NEXT_PUBLIC_SENTRY_PROJECT,
 
-      silent: !process.env.CI,
-      authToken: process.env.SENTRY_AUTH_TOKEN,
+    silent: !process.env.CI,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
 
-      // Enable source maps and React annotations
-      widenClientFileUpload: true,
-      reactComponentAnnotation: {
-        enabled: true,
-      },
+    // Enable source maps and React annotations
+    widenClientFileUpload: true,
+    reactComponentAnnotation: {
+      enabled: true,
     },
-  ),
+  }
 );
